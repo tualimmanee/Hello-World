0f6e733c5d81a033fb59a4262ab8251db5330662
tualimmanee <osuzumiyao@gmail.com>
2020-09-10 11:55:27 +0700

    test

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import CharacterCard from './CharacterCard';
+import './App.css';
+function App() {
+ return (
+ <div>
+ <CharacterCard value="h"/>
+ <CharacterCard value="i"/>
+ </div>
+ );
+}export default App;
